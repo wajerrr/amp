a93fd11cbf0a6d6f2bb5cc431c543cfa2c00de1d
Sergey Maksimov <sergey.maximov@yld.io>
2018-05-02 16:03:43 +0100

    Add prettier config + .editorconfig

--- src/server/App.jsx
+++ src/server/App.jsx
@@ -1,7 +1,11 @@
-
 import React from 'react';
- const App = ({title, url}) => (<div>
-     <h1>{title}</h1>
-     <a target="_blank" href={url}> <h2>{url}</h2> </a>
- </div>)
-export default App
+const App = ({ title, url }) => (
+  <div>
+    <h1>{title}</h1>
+    <a target="_blank" href={url}>
+      {' '}
+      <h2>{url}</h2>{' '}
+    </a>
+  </div>
+);
+export default App;
