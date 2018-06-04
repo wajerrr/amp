8db41d7b2fba6ecb8780e0f8f0d239afa4d7562f
PW <pw@PWs-MacBook-Pro.local>
2018-06-04 14:39:59 +0100

    enviroment fix

--- src/server/utils/environment-detection.js
+++ src/server/utils/environment-detection.js
@@ -1,4 +1,4 @@
 export const isProd =
-  process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'prod';
-export const isStage = process.env.NODE_ENV === 'stage';
-export const isDev = process.env.NODE_ENV === 'development';
+  process.env.ENV === undefined || process.env.ENV === 'prod';
+export const isStage = process.env.ENV === 'stage';
+export const isDev = process.env.ENV === 'development';
