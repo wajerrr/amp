6c6108f8bc6988db8c16f101d585ab7bcc215dfe
Yasmin Green <yasmin.green@btinternet.com>
2018-06-19 14:56:44 +0100

    refactoring WIP

--- src/app/components/navigation-menu/accordion-list.js
+++ src/app/components/navigation-menu/accordion-list.js
@@ -1,53 +1,101 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IconDown from '@economist/component-icon/lib/inline-icons/down';
 import color from '../../styles/color';
 import { LargeSubscribeButton } from '../styled-subscribe-button/styled-subscribe-button';
 import StyledLink from '../styled-link/styled-link';
 import StyledIconButton from '../styled-icon-button/styled-icon-button';
 
-const getListItemComponent = (item, styles) => {
-  let link = item.url && (
-    <StyledLink
-      href={item.url.canonical}
-      className={styles.StyledLink}
-      key={item.id}
-    >
-      {item.headline}
-    </StyledLink>
-  );
-  if (item.headline === 'Subscribe') {
-    link = <LargeSubscribeButton key={item.id} />;
-  }
-  return link || null;
-};
+function hasChildren(item) {
+  return item.hasPart && item.hasPart.parts && item.hasPart.parts.length > 0;
+}
+function isNestedList(list) {
+  let isNested = false;
+  list.map((listItem) => {
+    if (listItem.hasPart) {
+      isNested = hasChildren(listItem);
+    }
+  });
+  return isNested;
+}
 
-const AccordionListItem = ({ item, styles }) =>
-  item.hasPart ? (
-    <amp-accordion id="accordion">
-      <section className={styles.StyledSection}>
-        <h4 className={styles.StyledHead}>
-          <span>{item.headline}</span>
-          <StyledIconButton
-            className={styles.StyledIconButton}
-            onProps=""
-            icon="chevron"
-            iconColor={color.chicago}
-          />
-        </h4>
-        <div>
-          {item.hasPart.parts.map((partItem) => (
-            <AccordionListItem
-              item={partItem}
-              key={partItem.id}
-              styles={styles}
-            />
-          ))}
-        </div>
-      </section>
-    </amp-accordion>
+const HeaderSection = ({ title }, styles) => (
+  <h4 className={styles.StyledHead}>
+    <span>{title}</span>
+    <IconDown className={styles.StyledIconButton} />
+  </h4>
+);
+
+const TitleWithAccordionList = ({ title, list }, styles) => (
+  <amp-accordion id="accordion">
+    <section className={styles.StyledSection}>
+      <HeaderSection title={title} styles={styles} />
+      <div>
+        {list.hasPart.parts.map((listItem) => (
+          <StyledLink href={listItem.url.canonical}>
+            {listItem.headline}
+          </StyledLink>
+        ))}
+      </div>
+    </section>
+  </amp-accordion>
+);
+
+const HeaderLink = ({ link }, styles) =>
+  link.url ? (
+    <StyledLink href={link.url.canonical} className={styles.StyledHead}>
+      {link.headline}
+    </StyledLink>
   ) : (
-    getListItemComponent(item, styles)
+    <TitleWithAccordionList title={link.headline} list={link} />
   );
+
+const NestedAccordionList = ({ title, list }, styles) => (
+  <amp-accordion id="accordion">
+    <section className={styles.StyledSection}>
+      <HeaderSection title={title} styles={styles} />
+      <div>
+        {list.hasPart.parts.map(
+          (listItem) =>
+            hasChildren(listItem) ? (
+              <TitleWithAccordionList
+                title={listItem.headline}
+                list={listItem}
+              />
+            ) : (
+              <HeaderLink link={listItem} />
+            )
+        )}
+      </div>
+    </section>
+  </amp-accordion>
+);
+
+const AccordionListItem = ({ item, styles }) => {
+  switch (true) {
+    case hasChildren(item) && isNestedList(item.hasPart.parts):
+      return (
+        <NestedAccordionList
+          title={item.headline}
+          list={item}
+          styles={styles}
+        />
+      );
+      break;
+    case hasChildren(item):
+      return (
+        <TitleWithAccordionList
+          title={item.headline}
+          list={item}
+          styles={styles}
+        />
+      );
+      break;
+    default:
+      return <HeaderLink link={item} styles={styles} />;
+      break;
+  }
+};
 
 AccordionListItem.propTypes = {
   item: PropTypes.shape({}).isRequired,
