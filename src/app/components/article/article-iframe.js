111f3905e39a84c342ecc9e44b98961a16ed8f13
PW <pw@PWs-MacBook-Pro.local>
2018-06-19 09:02:06 +0100

    type in component name fixed

--- src/app/components/article/article-iframe.js
+++ src/app/components/article/article-iframe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import PlaceholderImage from '../placheolder-imge/placeholder-image';
+import PlaceholderImage from '../placeholder-image/placeholder-image';
 
 const ArticleIframe = ({ src, height }) => (
   <amp-iframe
