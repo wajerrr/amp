2b089e7318d9c509f5e5b261885da05bf2fcf51c
PW <pw@PWs-MacBook-Pro.local>
2018-06-26 15:14:30 +0100

    conflict resolve

--- src/app/components/article/article-text-builder.js
+++ src/app/components/article/article-text-builder.js
@@ -2,11 +2,8 @@ import React from 'react';
 import StyledLink from '../styled-link/styled-link';
 import StyledBlockquote from '../styled-block-quote/styled-block-quote';
 import Image from '../image/image';
-<<<<<<< HEAD
 import StyledInlineAd from '../styled-advert/styled-advert';
-=======
 import ArticleIframe from './article-iframe';
->>>>>>> iframe support added
 
 const buildArticleText = (childrenData = [], path = '') => {
   const getHTMLTag = (name, children, key) => {
